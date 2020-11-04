67e455b1e57cfbb2006c45766edac19b3645c785
uvirus <uvirus@ya.ru>
2020-11-04 11:58:22 +0500

    Убрал dio

--- lib/services/anivostApi.dart
+++ lib/services/anivostApi.dart
@@ -1,15 +1,18 @@
+import 'dart:convert';
 import 'package:aniapp/models/animeModel.dart';
-import 'package:dio/dio.dart';
+import 'package:http/http.dart' as http;
 
 class ApiAnimevost {
-  var dio = Dio();
   Future<List<Anime>> getLast(int page, int perPage) async {
     String url =
         'https://api.animevost.org/v1/last?page=$page&quantity=$perPage';
-    final Response response = await dio.get(url);
+    final http.Response response = await http.get(url);
 
+    var bytes = response.bodyBytes;
+    String responseString = utf8.decode(bytes);
+    var jsonResponse = jsonDecode(responseString);
     if (response.statusCode == 200) {
-      var obj = ResponseLast.fromJson(response.data);
+      var obj = ResponseLast.fromJson(jsonResponse);
       return obj.data;
     } else {
       throw Exception('Error loading anime');
@@ -20,14 +23,19 @@ class ApiAnimevost {
     String url = 'https://api.animevost.org/v1/playlist';
     Map<String, String> body = {'id': id.toString()};
     String encodedBody = body.keys.map((key) => "$key=${body[key]}").join("&");
-    final Response response = await dio.post(url,
-        data: encodedBody,
-        options: Options(
-            headers: {"Content-Type": "application/x-www-form-urlencoded"}));
+
+    var response = await http.post(url,
+        body: encodedBody,
+        headers: {"Content-Type": "application/x-www-form-urlencoded"});
+
+    var bytes = response.bodyBytes;
+    String responseString = utf8.decode(bytes);
 
     if (response.statusCode == 200) {
+      var jsonResponse = jsonDecode(responseString);
+
       List<AnimePlayItem> playList = List<AnimePlayItem>.from(
-          response.data.map((x) => AnimePlayItem.fromJson(x)));
+          jsonResponse.map((x) => AnimePlayItem.fromJson(x)));
       return playList;
     } else {
       throw Exception('Error loading playlist');
