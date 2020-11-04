9084f874b140ff40ffbce2afed9b17d44ecbd058
uvirus <uvirus@ya.ru>
2020-11-04 14:20:11 +0500

    refact

--- lib/services/anivostApi.dart
+++ lib/services/anivostApi.dart
@@ -1,44 +1,44 @@
 import 'dart:convert';
+import 'dart:typed_data';
 import 'package:aniapp/models/animeModel.dart';
 import 'package:http/http.dart' as http;
 
 class ApiAnimevost {
-  Future<List<Anime>> getLast(int page, int perPage) async {
-    String url =
-        'https://api.animevost.org/v1/last?page=$page&quantity=$perPage';
-    final http.Response response = await http.get(url);
+  String baseUrl = 'https://api.animevost.org/v1';
 
-    var bytes = response.bodyBytes;
-    String responseString = utf8.decode(bytes);
-    var jsonResponse = jsonDecode(responseString);
-    if (response.statusCode == 200) {
-      var obj = ResponseLast.fromJson(jsonResponse);
-      return obj.data;
-    } else {
-      throw Exception('Error loading anime');
-    }
+  Future<List<Anime>> getLast(int page, int perPage) async {
+    String url = '$baseUrl/last?page=$page&quantity=$perPage';
+
+    final http.Response response = await http.get(url);
+    var obj = ResponseLast.fromJson(responseHandling(response));
+    return obj.data;
   }
 
   Future<List<AnimePlayItem>> playList(int id) async {
-    String url = 'https://api.animevost.org/v1/playlist';
-    Map<String, String> body = {'id': id.toString()};
+    String url = '$baseUrl/playlist';
+    Map<String, int> body = {'id': id};
     String encodedBody = body.keys.map((key) => "$key=${body[key]}").join("&");
 
-    var response = await http.post(url,
+    http.Response response = await http.post(url,
         body: encodedBody,
         headers: {"Content-Type": "application/x-www-form-urlencoded"});
 
-    var bytes = response.bodyBytes;
-    String responseString = utf8.decode(bytes);
+    List<AnimePlayItem> playList = List<AnimePlayItem>.from(
+        responseHandling(response).map((x) => AnimePlayItem.fromJson(x)));
+    return playList;
+  }
 
+  ///Попытка универсального обработчика ответа
+  ///TODO разобраться что должно возвращаться
+  dynamic responseHandling(http.Response response) {
     if (response.statusCode == 200) {
-      var jsonResponse = jsonDecode(responseString);
-
-      List<AnimePlayItem> playList = List<AnimePlayItem>.from(
-          jsonResponse.map((x) => AnimePlayItem.fromJson(x)));
-      return playList;
+      Uint8List bytes = response.bodyBytes;
+      String responseString = utf8.decode(bytes);
+      dynamic jsonResponse = jsonDecode(responseString);
+      return jsonResponse;
     } else {
-      throw Exception('Error loading playlist');
+      ///TODO Надо что-то тут делать
+      throw Exception('Error http api');
     }
   }
 }
